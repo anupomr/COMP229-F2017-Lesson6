afbb53f22d45d89698a2b76bf6321f1f801c4d0b
Anupom Roy <anupomr@yahoo.com>
2017-10-27 15:07:27 -0400

    Added Login Logic

--- COMP229-F2017-Lesson6/Login.aspx.cs
+++ COMP229-F2017-Lesson6/Login.aspx.cs
@@ -5,12 +5,47 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+// required for Identity and OWIN Security
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+
 namespace COMP229_F2017_Lesson6
 {
     public partial class Login : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+
+        }
+
+        protected void LoginButton_Click(object sender, EventArgs e)
+        {
+            //Create new userStore and userManeger objects;
+            var userStore = new UserStore<IdentityUser>();
+            var userManager = new UserManager<IdentityUser>(userStore);
+
+            //serch for and create a new user Object
+            var user = userManager.Find(UserNameTextBox.Text, PasswordTextBox.Text);
+
+            //if a match  is found for the user
+            if(user != null )
+            {
+                //authenticate and login our user
+                var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+                var userIdentity = userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+
+                //sign in the user
+                authenticationManager.SignIn(new AuthenticationProperties() { IsPersistent = false }, userIdentity);
+
+                //redirect the user to the main menu
+                Response.Redirect("~/Contoso/MainManu.aspx");
+            }
+            else //user is not found
+            {
+                StatusLabel.Text = "Invalid Username or Password";
+                AlertFlash.Visible = true;
+            }
 
         }
     }
---
+++ COMP229-F2017-Lesson6/Logout.aspx
@@ -0,0 +1,3 @@
+﻿<%@ Page Title=" Logout" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Logout.aspx.cs" Inherits="COMP229_F2017_Lesson6.Logout" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
