d21018d6f4278ce53847cf067554ac16f339fb97
Anupom Roy <anupomr@yahoo.com>
2017-10-27 17:05:21 -0400

    added Logout and register

--- COMP229-F2017-Lesson6/Register.aspx.cs
+++ COMP229-F2017-Lesson6/Register.aspx.cs
@@ -5,6 +5,11 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+//required for the Identity and OWIN Security
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin.Security;
+
 namespace COMP229_F2017_Lesson6
 {
     public partial class Register : System.Web.UI.Page
@@ -16,12 +21,46 @@ namespace COMP229_F2017_Lesson6
 
         protected void CancelButton_Click(object sender, EventArgs e)
         {
-
+            // redirect back to the Default page
+            Response.Redirect("~/Default.aspx");
         }
 
         protected void RegisterButton_Click(object sender, EventArgs e)
         {
+            //Create new userStore and userManeger objects;
+            var userStore = new UserStore<IdentityUser>();
+            var userManager = new UserManager<IdentityUser>(userStore);
 
+            //Creat a new user object 
+            var user = new IdentityUser()
+            {
+                UserName = UserNameTextBox.Text,
+                PhoneNumber=PhoneNumberTextBox.Text,
+                Email=EmailTextBox.Text
+            };
+
+            // create a new user in the database and store the results
+            IdentityResult result = userManager.Create(user, PasswordTextBox.Text);
+
+            // check if successfully registered?
+            if (result.Succeeded)
+            {
+                //authenticate and login our new user
+                var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+                var userIdentity = userManager.CreateIdentity(user, DefaultAuthenticationTypes.ApplicationCookie);
+
+                //Sign in user
+                authenticationManager.SignIn(new AuthenticationProperties() { }, userIdentity);
+
+                // Redirect  to the main Maenu Page
+                Response.Redirect("~/Contoso/MainMenu.aspx");
+            }
+            else
+            {
+                //display error in AlertFlash div
+                StatusLabel.Text = result.Errors.FirstOrDefault();
+                AlertFlash.Visible = true;
+            }
         }
     }
 }
